e44a56be1ef98171c5c657dad47d456f5a138d1b
KIT <nikitabit1990@gmail.com>
2023-01-09 13:18:09 +0700

    Refactoring and comments

--- eathAnimation/main.py
+++ eathAnimation/main.py
@@ -1,10 +1,11 @@
-from arts import slides
 from time import sleep
+
+from arts import slides
 
 # Make slide animation
 for slide in slides:
     print('\033[H')  # clear terminal
-    print('\u001b[32m', slide) #set text color
+    print('\u001b[32m', slide)  # set text color and animate slide
     sleep(0.2)
 
-print('\u001b[0m')
+print('\u001b[0m')  # return default text color
