cf012f3b6f9ad9d4882c2f946ccd1e50d88a7a8c
KIT <nikitabit1990@gmail.com>
2023-01-09 22:04:35 +0700

    Comments and refactor

--- easyLinks/main.py
+++ easyLinks/main.py
@@ -1,13 +1,15 @@
+import os
+
 import requests
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 token = os.environ['TOKEN']
 
+
 def main():
     link = input('Your link: ')
-
+    # Проверяем какую ссылку ввел пользователь, если короткую показываем сколько было кликов
     if is_bitlink(link):
         try:
             click_count = count_clicks(link)
@@ -16,6 +18,7 @@ def main():
             exit()
         print(f'This link has {click_count} clicks')
 
+    # Если длинную, то сокращаем
     else:
         try:
             bitlink = shorten_link(link)
@@ -26,6 +29,7 @@ def main():
         print('Bitlink: ', bitlink)
 
 
+# Функция возвращает  ссылку в сокращенном виде, использую внешний API bitlink
 def shorten_link(link):
     url = "https://api-ssl.bitly.com/v4/bitlinks"
     headers = {"Authorization": f"Bearer {token}"}
@@ -38,6 +42,7 @@ def shorten_link(link):
     return bitlink.json()['link']
 
 
+# Функция возвращает количество переходов по входящей ссылке
 def count_clicks(bitlink):
     bitlink = bitlink.split("//")[1] if '//' in bitlink else bitlink
 
@@ -53,6 +58,7 @@ def count_clicks(bitlink):
     return clicks.json()["total_clicks"]
 
 
+# Функция проверяет переданную ссылку: сокращенная она или нет
 def is_bitlink(link):
     link = link.split("//")[1] if '//' in link else link
     headers = {"Authorization": f"Bearer {token}"}
@@ -61,4 +67,6 @@ def is_bitlink(link):
     clicks = requests.get(url, headers=headers)
     return True if clicks.ok else False
 
-main()
+
+if __name__ == "__main__":
+    main()
